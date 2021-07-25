2a1e494eefbacf65b6deec1ee97a27f0f3da96ab
Mehdi Mirzapour <mehdi.mirzapour@gmail.com>
2021-07-25 04:02:27 +0200

    version

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup, find_packages
 
-with open("requirements.txt") as f:
+with open("/home/requirements.txt") as f:
     required = f.read().splitlines()
 
 setup(
     name="lingx",
-    version="0.1.0",
+    version="0.1.2",
     description="A library for introducing state-of-the-art metrics on measuring linguistic complexity",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     author="Mehdi Mirzapour",
     author_email="mehdi.mirzapour@gmail.com",
-    url="https://bitbucket.org/contentside/lingx",
+    url="https://github/ContentSide/lingx",
     packages=find_packages(exclude="tests"),  # same as name
     license="MIT",
     install_requires=required,
     include_package_data=True,
-    python_requires=">=3.6",
+    python_requires=">=3.6"
 )
