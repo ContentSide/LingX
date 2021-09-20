9d7b65cd75d5fa771abe1b0ae5d515695c637896
Mehdi Mirzapour <mehdi.mirzapour@gmail.com>
2021-09-20 21:34:07 +0200

    critical error fixed

--- lingx/utils/download_lang_models.py
+++ lingx/utils/download_lang_models.py
@@ -2,5 +2,5 @@ import lingx.core.lang_model as lm
 
 
 print("Downloading Language Models:")
-lm.download_stanza_model("en")
+lm.download_stanza_model("en", package="partut")
 lm.download_stanza_model("zh-hans")
